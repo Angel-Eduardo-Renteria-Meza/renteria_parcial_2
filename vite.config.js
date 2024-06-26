833826a0989ce267054186d53183b37a0550028b
Angel-Eduardo-Renteria-Meza <ingeduardorenteriamz@gmail.com>
2024-06-26 12:25:06 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/renteriaParcial2/', // Asegura que los recursos se sirvan desde el subdirectorio correcto
+  base: '/', // Asegura que los recursos se sirvan desde el subdirectorio correcto
 })
